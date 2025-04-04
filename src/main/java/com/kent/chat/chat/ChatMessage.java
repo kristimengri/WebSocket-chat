d2118602db498ce66a51f05b75aea88d5f38d158
kristimengri <kristimengri@gmail.com>
2025-04-04 11:53:24 +0200

    websocket configuring and adding disconnect event listener

---
+++ src/main/java/com/kent/chat/chat/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.kent.chat.chat;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
